079f016656733c258fc4312bf8729576144c7017
Gamestonk2 <david.cho1@students.mq.edu.au>
2022-09-26 22:47:26 +1000

    .gitignore added

--- COMP2160 Week 8/Assets/Scripts/Player.cs
+++ COMP2160 Week 8/Assets/Scripts/Player.cs
@@ -12,7 +12,6 @@ public class Player : MonoBehaviour
     [SerializeField]private float turningRate;
 
     [SerializeField] private float speed;
-    private bool isCollide = false;
 
     // Start is called before the first frame update
     void Start()
